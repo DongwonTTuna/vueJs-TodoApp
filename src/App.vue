f26f58523839cc507eeba8de5e7218d2c9ba4870
DongwonTuna <ttuna0790@gmail.com>
2023-06-02 13:47:09 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="mainApp">
+    <button class="mainApp--log">log</button>
+    <div class="mainApp__clock">
+      <p class="mainApp__clock--currentDate">{{ formattedDate }}</p>
+      <p class="mainApp__clock--currentTime">{{ formattedTime }}</p>
+    </div>
+    <div class="todo">
+      <div class="todo__text">Todo List</div>
+      <div class="todo__items">
+        <TodoItem
+          v-for="item in 5"
+          :key="item"
+          :todoItem="{
+            title: 'title',
+            description: 'description',
+            expireDate: new Date()
+          }"
+          />
+        <img
+          class="addTodo"
+          loading="lazy"
+          src="/images/todo/addNew.png"
+          alt="addItem"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import TodoItem from "./components/todoItem.vue";
+const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+export default {
+  name: "App",
+  data() {
+    return {
+      currentDate: new Date(),
+    };
+  },
+  components: {
+    TodoItem,
+  },
+  computed: {
+    formattedDate() {
+      const { currentDate } = this;
+      return `${currentDate.getDate()}.${(currentDate.getMonth() + 1)
+        .toString()
+        .padStart(2, "0")}.${currentDate.getFullYear()} (${
+        daysOfWeek[currentDate.getDay()]
+      })`;
+    },
+    formattedTime() {
+      const { currentDate } = this;
+      return currentDate.toLocaleString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        hour12: true,
+      });
+    },
+  },
+  mounted() {
+    setInterval(() => {
+      this.currentDate = new Date();
+    }, 1000);
+  },
+};
+</script>
+
+<style>
+.addTodo {
+  cursor: pointer;
+}
+</style>
---
+++ src/components/todoItem.vue
@@ -0,0 +1,60 @@
+<template>
+    <table class="todoItem" >
+      <tr>
+        <td class="todoItem__title">
+          {{ todoItem.title }}
+        </td>
+        <td class="todoItem__description">
+          {{ todoItem.description }}
+        </td>
+        <td>
+          <div class="todoItem__expireDate">
+            {{
+              `${todoItem.expireDate.getDate()}.${(
+                todoItem.expireDate.getMonth() + 1
+              )
+                .toString()
+                .padStart(2, "0")}.${todoItem.expireDate.getFullYear()} (${
+                daysOfWeek[todoItem.expireDate.getDay()]
+              })`
+            }}
+            <br />
+            {{
+              todoItem.expireDate.toLocaleString("en-US", {
+                hour: "numeric",
+                minute: "numeric",
+                hour12: true,
+              })
+            }}
+          </div>
+        </td>
+      </tr>
+    </table>
+</template>
+
+<style></style>
+
+<script lang="ts">
+import { PropType } from "vue";
+
+interface todoItem {
+  title: string;
+  description: string;
+  expireDate: Date;
+}
+export default {
+  name: "TodoItem",
+  data() {
+    return {
+      daysOfWeek: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+      // Other data properties
+    };
+  },
+  props: {
+    todoItem: {
+      type: Object as PropType<todoItem>,
+      required: true,
+    },
+  },
+};
+</script>
