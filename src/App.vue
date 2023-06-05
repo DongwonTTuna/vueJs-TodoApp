a327a075dd8ad90f820c4abcfc7d2b3a3a8d74ef
DongwonTuna <ttuna0790@gmail.com>
2023-06-05 15:06:57 +0900

    added several functions for todoapp

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,36 @@
     </div>
     <div class="todo">
       <div class="todo__text">Todo List</div>
+      <div class="todo__filter">
+        <div class="todo__filter--text">filtering</div>
+        <table class="filter__table">
+          <tr class="filter__table--category">
+            <td class="filter__table--category--text">Category</td>
+            <td class="filter__table--category">
+              <div
+                v-for="category in categories"
+                class="filter__table--category--item"
+              >
+                {{ category }}
+              </div>
+            </td>
+          </tr>
+          <tr class="filter__table__progress">
+            <td class="filter__table--progress--text">Progress</td>
+            <td class="filter__table--progress">
+              <div @click="filterTodoByProgress(0)" class="filter__table--progress--undone">Undone</div>
+              <div @click="filterTodoByProgress(1)" class="filter__table--progress--finished">Finished</div>
+              <div @click="filterTodoByProgress(2)" class="filter__table--progress--progressing">
+                Progressing
+              </div>
+              <div @click="filterTodoByProgress(3)" class="filter__table--progress--beforeStart">
+                BeforeStart
+              </div>
+            </td>
+          </tr>
+        </table>
+      </div>
+
       <div class="todo__items">
         <TodoItem
           v-for="item in 5"
@@ -16,13 +46,9 @@
             description: 'description',
             expireDate: new Date(),
             progress: 'beforeStart',
-            category: [
-              { name: 'category1', color: '#f3cc92' },
-              { name: 'category2', color: '#f3ecee' },
-              { name: 'category3', color: '#636063' },
-            ],
+            category: categories,
           }"
-          />
+        />
         <img
           class="addTodo"
           loading="lazy"
@@ -35,7 +61,22 @@
 </template>
 
 <script lang="ts">
-import TodoItem from "./components/todoItem.vue";
+
+import TodoItemComponent from "./components/todoItem.vue";
+
+
+type Progress = "Undone" | "Finished" | "Progressing" | "beforeStart";
+interface Category {
+  color: string;
+  name: string;
+}
+interface TodoItem {
+  title: string;
+  description: string;
+  category: Array<Category>;
+  progress: Progress;
+  expireDate: Date;
+}
 const daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
 export default {
@@ -43,10 +84,12 @@ export default {
   data() {
     return {
       currentDate: new Date(),
+      categories: [],
+      todoItems: []
     };
   },
   components: {
-    TodoItem,
+    "TodoItem" : TodoItemComponent,
   },
   computed: {
     formattedDate() {
@@ -71,6 +114,27 @@ export default {
       this.currentDate = new Date();
     }, 1000);
   },
+  methods: {
+    addTodo(todoIndex: number, todoItem: TodoItem) {
+      console.log("addTodo");
+    },
+    removeTodo(todoIndex: number, todoItem: TodoItem) {
+      console.log("removeTodo");
+    },
+    editTodo(todoIndex: number, todoItem: TodoItem) {
+      console.log("editTodo")
+    },
+    filterTodoByProgress(progress: number){
+      
+      switch(progress){
+        case 0: return console.log("Undone");
+        case 1: return console.log("Finished");
+        case 2: return console.log("Progressing");
+        case 3: return console.log("beforeStart");
+      }
+
+    }
+  }
 };
 </script>
 
